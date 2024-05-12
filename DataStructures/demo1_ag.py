7f251887bf64a682feae1cfe9797e6cb58484b45
ASHWIN <90372256+Ashwin0132@users.noreply.github.com>
2024-05-12 10:46:51 +0530

    Create demo1_ag.py(list operations)

---
+++ DataStructures/demo1_ag.py
@@ -0,0 +1,8 @@
+'''LIST OPERATIONS'''
+
+list1 = ['HP', 'DELL', 'LENOVO', 'APPLE', 'ACER']
+print('Original list:', list1)
+list1.append('ASUS')
+print('Updated list:',list1)
+print('The length of the list is: ',len(list1))
+
