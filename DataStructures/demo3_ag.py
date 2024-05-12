25ec01a140b499f75c8725564c41da9bee317a4f
ASHWIN <90372256+Ashwin0132@users.noreply.github.com>
2024-05-12 21:20:11 +0530

    Create demo3_ag.py

---
+++ DataStructures/demo3_ag.py
@@ -0,0 +1,9 @@
+'''STRING OPERATIONS'''
+
+my_str = "artificial intelligence and generative AI"
+print(my_str.capitalize())
+print(my_str.title()) #titlecase
+print(my_str.index("in")) #index
+print(my_str.isalpha()) #alpha or not
+print(my_str.upper()) #uppercase
+print(my_str.replace("generative", "gen")) #replace
