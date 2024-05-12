8f9a90aabf3cd4396876679bd4a24f03dd65248e
ASHWIN <90372256+Ashwin0132@users.noreply.github.com>
2024-05-12 21:37:53 +0530

    Create demo4_ag.py

---
+++ DataStructures/demo4_ag.py
@@ -0,0 +1,10 @@
+'''DICTIONARY'''
+
+sub_names = {"Computer Science" : "Data Science", 
+             "Biology": "Bioinformatics", 
+             "Aeronautical": "Pilot"
+             }
+print(sub_names)
+print(sub_names["Biology"]) #accessing index
+sub_names["Business"]="Business Analytics" # adding extra dictionar element
+print(sub_names)
